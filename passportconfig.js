f625fd224c0307c69380ad36a4cc63693199bbb0
EJIKE <perpetualezeifeanyi@gmail.com>
2023-07-31 23:41:46 +0100

    .

--- passportconfig.js
+++ passportconfig.js
@@ -37,6 +37,24 @@ function initialize(passport) {
         )
 
     }
+    module.exports = function (passport) {
+        passport.use(
+            new localStrategy((username, password, done) => {
+                User.findOne({ username: username }, (err, user) => {
+                    if (err) throw err;
+                    if (!user) return done(null, false);
+                    bcrypt.compare(password, user.password, (err, result) => {
+                        if (err) throw err;
+                        if (result === true) {
+                            return done(null, user);
+                        } else {
+                            return done(null, false);
+                        }
+                    });
+                });
+            })
+        );
+
     passport.use(
         new LocalStrategy({
             usernameField: "email",
