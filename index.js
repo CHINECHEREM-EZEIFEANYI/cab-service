7d1b9b8d61bcb612b45a5f8f836c5b63c2bb1fce
EJIKE <perpetualezeifeanyi@gmail.com>
2023-08-20 20:41:12 +0100

    nonso's commit

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 const session = require("express-session");
 const { authUser, authRole } = require("./src/basicAuth");
 const { v4: uuidv4 } = require("uuid");
 const apiKey = process.env.API_KEY;
 
 app.use(
-  session({
-    genid: function (req) {
-      return uuidv4();
-    },
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })
+    session({
+        genid: function (req) {
+            return uuidv4();
+        },
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: true,
+    })
 );
 app.use(express.json());
 app.use(bodyParser.json());
